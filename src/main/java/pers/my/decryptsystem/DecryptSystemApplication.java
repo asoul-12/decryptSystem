dca306fce2fbedff4a8eac06f62e13d220390503
liquor <1297589760@qq.com>
2022-04-30 23:57:30 +0800

    first commit

---
+++ src/main/java/pers/my/decryptsystem/DecryptSystemApplication.java
@@ -0,0 +1,13 @@
+package pers.my.decryptsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecryptSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecryptSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pers/my/decryptsystem/DecryptSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.my.decryptsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecryptSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
